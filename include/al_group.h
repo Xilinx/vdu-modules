2fbb2f6356e086955bd625afb842e19b85aa8d5c
Arthur Vinchon <arthur.vinchon@allegrodvt.com>
2022-12-16 15:22:56 +0100

    fix: use mutex instead of spinlock
    Signed-off-by: Arthur Vinchon <arthur.vinchon@allegrodvt.com>

--- include/al_group.h
+++ include/al_group.h
@@ -18,14 +18,14 @@
 #ifndef __AL_GROUP__
 #define __AL_GROUP__
 
-#include <linux/spinlock.h>
+#include <linux/mutex.h>
 
 #include "mcu_interface.h"
 #include "al_user.h"
 
 struct al5_group {
 	struct mcu_mailbox_interface *mcu;
-	spinlock_t lock;
+	struct mutex lock;
 
 	int max_users_nb;
 	struct al5_user **users;
@@ -38,7 +38,7 @@ void al5_group_deinit(struct al5_group *group);
 
 int al5_group_bind_user(struct al5_group *group, struct al5_user *user);
 void al5_group_unbind_user(struct al5_group *group, struct al5_user *user);
-void al5_group_unbind_user_without_group_spinlock(struct al5_group *group, struct al5_user *user);
+void al5_group_unbind_user_without_group_lock(struct al5_group *group, struct al5_user *user);
 
 struct al5_user *al5_group_user_from_uid(struct al5_group *group, int user_uid);
 struct al5_user *al5_group_user_from_chan_uid(struct al5_group *group,
